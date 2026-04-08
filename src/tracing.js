efbece3dbf1822e3cec17cb07fe7fbcedcdabea7
kihyun <jsh99875@gmail.com>
2026-04-08 14:29:35 +0900

    feat: add Prisma instrumentation for DB traces

--- src/tracing.js
+++ src/tracing.js
@@ -1,13 +1,14 @@
 const { NodeSDK } = require('@opentelemetry/sdk-node');
 const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
 const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
+const { PrismaInstrumentation } = require('@prisma/instrumentation');
 
 const sdk = new NodeSDK({
   serviceName: 'bookmark-api',
   traceExporter: new OTLPTraceExporter({
     url: process.env.OTEL_EXPORTER_OTLP_ENDPOINT || 'http://localhost:4318/v1/traces',
   }),
-  instrumentations: [getNodeAutoInstrumentations()],
+  instrumentations: [getNodeAutoInstrumentations(), new PrismaInstrumentation()],
 });
 
 sdk.start();
